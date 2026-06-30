7ffb2f1d9b41c9638f2cfc3cdf2ec313efbeec7f
marsil1990 <ma.silveram@gmail.com>
2026-06-30 12:13:22 -0300

    week1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,12 +9,23 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+
+
+
+
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //plan:
+        // Create a new array called result with the size given by length
+        // Use a for loop to go each index of the array
+        // Since arrays start index 0, use (i+1)*number to calculate the correct multiple
+        // Store each multiple in the correct position of the result array
+        // Return the completed array
         var result = new double[length];
         for (int i = 0; i < length; i++)
         {
@@ -31,12 +42,21 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Plan:
+        //Create a new empty list called result.
+        //Find the starting position of the elements that need to move to the front. This position is data.Count - amount.
+        //Starting from that position, copy each element from data to result until the end of the list.
+        //Then go back to the beginnig of data to result until the position before data.Count - amount.
+        //At this point, rsult has the rotated order.
+        //Copy each element from result back into data.
         List<int> result = new List<int>();
         for (int i = data.Count - amount; i < data.Count; i++)
         {
